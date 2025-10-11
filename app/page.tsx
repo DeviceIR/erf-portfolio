46b80f2f1ff434cbba8db40ef7b3c31ba70e0286
DeviceIR <erfanbastani1201@gmail.com>
2025-10-11 13:50:23 +0330

    2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/ModeToggle";
+
 export default function Home() {
-  return <main className="flex items-center justify-center h-screen"></main>;
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <Button>click me</Button>
+      <ModeToggle></ModeToggle>
+    </main>
+  );
 }
